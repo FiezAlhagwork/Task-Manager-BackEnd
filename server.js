16a1aa9484f17a015c7e5c39b4de68d9ca1fb420
fiezalhag <fiezalhag@gmail.com>
2025-09-28 00:18:14 -0700

    Configure MongoDB connection settings

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
-
 //Middleware to handle CORS
 app.use(
-    cors({
-        origin:process.env.CLIENT_URL || "*", 
-        methods:["GET","POST","PUT", "DELETE"],
-        allowedHeaders:["Content-Type", "Authorization"]
-    })
-)
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+//Connect DataBase
+connectDB();
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Route
 // app.use("/api/auth" , authRoutes)
@@ -25,5 +28,5 @@ app.use(express.json())
 // app.use("/api/report", reportRoutes)
 
 //Start Server
-const PORT = process.env.PORT || 5000
-app.listen(PORT , () => console.log(`http://localhost:${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
