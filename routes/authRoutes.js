a934cfbab0f0b9a3bb4d01ef5f7293e3d6ff71a7
fiezalhag <fiezalhag@gmail.com>
2025-10-01 03:55:17 -0700

    Add upload middleware using Multer for image uploads (TODO: integrate Cloudinary)

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,30 @@
 const express = require("express");
-const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controllers/authController");
+const upload = require("../Middleware/uploadMiddleware");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/authController");
 const { protect } = require("../Middleware/authMiddleware");
 
-const router = express.Router()
 
-router.post("/register", registerUser );
-router.post("/login", loginUser );
-router.get("/profile", protect, getUserProfile)
-router.put("/profile",protect, updateUserProfile)
+const router = express.Router();
 
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded", error: true });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res
+    .status(200)
+    .json({ message: "Image uploaded successfully", imageUrl, error: false });
+});
 
-module.exports = router
+module.exports = router;
